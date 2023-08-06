99b50e1e5381eb4bd58b3e876f9a6546740ec6e6
pavithracr1111 <pavithracr2001@gmail.com>
2023-08-06 23:40:13 +0530

    student

---
+++ student.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+static int i=0;
+class Student
+{
+    private: 
+    int rollNo;
+    char name[20];
+
+    public:
+
+    //setters
+    void setrollNo()
+    {
+        rollNo=++i;
+    }
+    
+    //getters
+    int getrollNo()
+    {
+        return rollNo;
+    }
+    
+    //setters
+    void setname(char *n)
+    {
+        strcpy (name, n);
+    }
+    //getters
+    char *getname()
+    {
+        return name;
+    }
+    
+ };
+
+ int main()
+ {
+    Student s1,s2;
+    s1.setrollNo();
+    s2.setrollNo();
+
+    char temp1[20]="PAvi", temp2[20]="Rishav";
+
+    
+    s1.setname(temp1);
+    s2.setname(temp2);
+
+    cout<< " The rollno of " << s1.getname()<<" is : "<< s1.getrollNo()<<endl;
+    cout<< " The rollno of " << s2.getname() << " is : "<< s2.getrollNo()<<endl;
+    
+   
+    return 0;
+ }
--- test.cpp
+++ test.cpp
@@ -1,5 +1,47 @@
 #include<iostream>
-int main
+using namespace std;
+class Rectangle
 {
-    std::cout<< "Hello rishav";
+    private:
+    int length,breadth;
+
+    public:
+
+    //setters
+    void setLength(int len)
+    {
+        length=len;
+    }
+    //setters
+    void setBreadth(int bre)
+    {
+        breadth=bre;
+    }
+    //getters
+    int getLength()
+    {
+        return length;
+    }
+    //getters
+    int getBreadth()
+    {
+        return breadth;
+    }
+    
+    int area()
+    {
+        return length*breadth;
+    }
+
+    };
+int main()
+{
+    Rectangle recObj;
+    recObj.setLength(10);
+    recObj.setBreadth(20);
+
+    cout << "The length is " << recObj.getLength()<< endl;
+    cout << "The breadth is " << recObj.getBreadth()<< endl;
+    cout << "The area of recatngle is" << recObj.area()<<endl;
+    return 0;
 }
